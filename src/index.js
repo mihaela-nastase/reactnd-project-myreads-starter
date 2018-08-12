7694fa95ec679b222cd575b12903355beecf64e7
Mihaela-Cristina Nastase <mihaela.nastase93@yahoo.ro>
2018-08-12 15:17:11 +0300

    Add react-router-dom in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+/*We wrap the entire app in a BrowserRouter. This just sets up the router to be able to work with all the other components imported. And then it also listens to the URL, and notifies those other components when the URL changes.*/
+ReactDOM.render(
+	<BrowserRouter><App /></BrowserRouter>,
+	document.getElementById('root'))
