bed01207bee18a7d3e69f5772eef1aeacaba5f24
Mihaela-Cristina Nastase <mihaela.nastase93@yahoo.ro>
2018-08-13 13:05:24 +0300

    Add debounce

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -3,6 +3,26 @@ import { Link } from "react-router-dom";
 import BookItem from "./BookItem";
 import * as BooksAPI from "./BooksAPI";
 
+/*Source: https://davidwalsh.name/javascript-debounce-function */
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 class SearchBooks extends Component {
   state = {
     query: "",
@@ -10,9 +30,11 @@ class SearchBooks extends Component {
   };
 
   /*Whenever the input field changes, we want to go ahead and update our query. So I made an update query method. This takes an end of query and updates the state. We’re not going to be updating the new state based off the previous state, so we can just pass this object. We check the shelves of the books found with the addShelf method. If no books are found, we clear the array. */
-  updateQuery = query => {
+  /*Debounce solution source: https://stackoverflow.com/questions/45447602/react-call-api-filter-out-undefined-promises */
+  updateQuery = debounce((query) => {
     this.setState({ query: query });
     if (query) {
+		console.log(query);
       let searchResults = [];
       BooksAPI.search(query, 20).then(results => {
         if (results && results.length) {
@@ -26,14 +48,15 @@ class SearchBooks extends Component {
         }
       });
     } else {
+	  this.updateQuery('');
       this.setState({ books: [] });
 	}
-  };
+  }, 100)
+  
+  componentWillUnmount(){
+	this.updateQuery.cancel()
+  }
 
-  clearQuery = () => {
-    this.setState({ query: "" });
-  };
- 
   /*We verify if the book id corresponds to a book on the main page, and update its shelf. If it doesn't, we set the shelf to 'none'. Source for the solution: https://github.com/dandenney/my-reads/blob/master/src/SearchBooks.js */
   checkShelf(result) {
     let hasShelf = this.props.books.filter(book => book.id === result.id);
